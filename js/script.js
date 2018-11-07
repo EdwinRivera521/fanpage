05dbdfcd960ae7f6f9bf84a7027bdbafa9fe9c93
Edwin Rivera <edwin.rivera52102@gmail.com>
2018-11-07 22:47:18 +0000

    added click handlers

--- js/script.js
+++ js/script.js
@@ -4,4 +4,18 @@
 $('body').click(function(){
     
 });
+$(".thumbnail-image").hide();
+$(".htext").hide();
 
+$("button").click(function(){
+    $("#thumbnail-image").hide();
+    $("#base").hide;
+    $("#1").show();
+    $("#Ssj").show;
+});
+$("button").dblclick(function(){
+    $("#1").hide();
+    $("#Ssj").hide;
+    $("#2").show();
+    $("#Ssj").show;
+});
